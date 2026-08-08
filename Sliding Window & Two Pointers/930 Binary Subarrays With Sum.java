c607c355a0559f6938c0ef82fe903daae5b13b38
Ankit-pro908 <ankitpattanayak908@gmail.com>
2026-08-08 14:23:45 +0530

    Solved LeetCode 930

---
+++ Sliding Window & Two Pointers/930 Binary Subarrays With Sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums,goal-1);
+
+    }
+    private int atMost(int[] nums, int goal) {
+        if(goal < 0){
+            return 0;
+        }
+        int r = 0; int l = 0;
+        int sum = 0;
+        int cnt = 0;
+        int n = nums.length;
+        while(r<n){
+            sum = sum + nums[r];
+        while(sum > goal){
+            sum = sum - nums[l];
+            l++;
+            }
+            cnt = cnt + r-l+1;
+            r++;
+        }
+        
+        return cnt;
+    }
+}
